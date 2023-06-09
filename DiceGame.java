520cceeace28d3ff12f5ce4c98dd0d8432dd29b2
Aku Haga <s1300102@u-aizu.ac.jp>
2023-06-09 12:36:32 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -5,6 +5,9 @@ class DiceGame{
 	int die1 = random.nextInt(6) + 1;
 	int die2 = random.nextInt(6) + 1;
 	int sum = die1 + die2;
+	System.out.println("What is your name?");
+	System.out.println("> Aku");
+	System.out.println("Hello, AKu!");
 	System.out.println("Rolling dice...");
 	System.out.println("Die 1 : " + die1);
 	System.out.println("Die 2 : " + die2);
