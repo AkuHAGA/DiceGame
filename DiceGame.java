e59473d8b61b4e511a5cab7c56bfe7dfa2441b3b
Aku Haga <s1300102@u-aizu.ac.jp>
2023-06-14 14:10:06 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -9,5 +9,7 @@ class DiceGame{
 	System.out.println("Die 1 : " + die1);
 	System.out.println("Die 2 : " + die2);
 	System.out.println("Total value: " + sum);
+	if(sum > 7)System.out.println("Aku won!");
+	else System.out.println("Aku won");
     }
 }
